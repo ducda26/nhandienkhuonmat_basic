b3562834fe1575b29901bd796fc8950709638585
ducda26 <doananhduc26@gmail.com>
2023-10-15 23:11:53 +0700

    Hien thi anh co dinh voi 1 kich thuoc

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import cv2
 import face_recognition
 
 # Step 01:
-imgElon = face_recognition.load_image_file("./pic/elon musk.jpg")
+imgElon = face_recognition.load_image_file("./pic/thien_01.jpg")
 imaElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
-imgCheck = face_recognition.load_image_file("./pic/elon check.jpg")
+imgCheck = face_recognition.load_image_file("./pic/thien_02.jpg")
 imgCheck = cv2.cvtColor(imgCheck, cv2.COLOR_RGB2BGR)
 
 # Step 02: Xác định vị trí khuôn mặt cần nhận dạng
 faceloc = face_recognition.face_locations(
     imgElon)[0]  # Vì có 1 bức ảnh nên index = 0
+
 print(faceloc)  # (y1,x2,y2,x1)
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 cv2.rectangle(imgElon, (faceloc[3], faceloc[0]),
@@ -17,10 +18,11 @@ cv2.rectangle(imgElon, (faceloc[3], faceloc[0]),
 
 
 faceCheck = face_recognition.face_locations(imgCheck)[0]
+print(faceCheck)
+
 encodeCheck = face_recognition.face_encodings(imgCheck)[0]
 cv2.rectangle(imgCheck, (faceCheck[3], faceCheck[0]),
               (faceCheck[1], faceCheck[2]), (255, 0, 255), 2)
-
 
 # So sánh hình ảnh mã hóa với các điểm trên khuôn mặt có khớp không
 results = face_recognition.compare_faces([encodeElon], encodeCheck)
@@ -30,6 +32,13 @@ print(results)
 # khoảng cách (sai số ) giữa các bức ảnh là bao nhiêu?
 faceDis = face_recognition.face_distance([encodeElon], encodeCheck)
 print(results, faceDis)
+
+# Chỉnh lại kích thước của ảnh để hiển thị với kích thước cố định
+desired_width = 450
+imgElon = cv2.resize(imgElon, (desired_width, int(
+    desired_width * imgElon.shape[0] / imgElon.shape[1])))
+imgCheck = cv2.resize(imgCheck, (desired_width, int(
+    desired_width * imgCheck.shape[0] / imgCheck.shape[1])))
 
 
 # Hiển thị kết quả lên màn hình
